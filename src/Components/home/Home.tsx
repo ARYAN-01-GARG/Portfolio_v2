483234d6096e845fe3b34e79a04da1f181ae7d87
ARYAN-01-GARG <aryangarg30842@gmail.com>
2025-06-15 16:33:41 +0530

    Add : Added All in Home

--- src/Components/home/Home.tsx
+++ src/Components/home/Home.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import ContactSection from './ContactSection'
+import WhoAmI from './WhoAmI'
+import HeroSection from './HeroSection'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='min-h-screen'>
+      <HeroSection />
+      <WhoAmI />
+      <ContactSection />
+    </div>
   )
 }
