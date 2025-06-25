14791059f52b3c175cdcb8cede85f9c23cc8e94d
ARYAN-01-GARG <aryangarg30842@gmail.com>
2025-06-26 01:57:48 +0530

    About section

---
+++ src/Components/home/components/About.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function About() {
+  return (
+    <section className="min-h-screen flex items-center justify-center bg-[#080E21] py-12 lg:p-12">
+        hello
+    </section>
+  )
+}
